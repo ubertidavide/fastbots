fa6a3747bfd51266f5658aa67c88c996c955dbad
Davide Uberti <24529587+ubertidavide@users.noreply.github.com>
2023-11-23 20:18:04 +0100

    Update page.py

--- fastbots/page.py
+++ fastbots/page.py
@@ -7,26 +7,47 @@ from selenium.webdriver.common.by import By
 from fastbots.bot import Bot
 from fastbots import config
 
-
 logger = logging.getLogger(__name__)
-
 
 class Page(ABC):
     """
     Page
 
-    A web page blueprint used to forward pages and
-    make a series of actions on it.
+    A blueprint for web pages used to navigate and perform actions on them.
+
+    Attributes:
+        _bot (Bot): The bot instance associated with the page.
+        _page_name (str): The name of the page.
+        _page_url (str): The URL of the page.
+
+    Methods:
+        __init__(bot: Bot, page_name: str = 'page_name'): Initializes the Page class.
+        bot: Gets the associated bot instance.
+        __locator__(locator_name: str) -> tuple: Utility method to load a locator.
+        forward() -> Union[Type['Page'], None]: Represents a series of actions on the page.
+
+    Example:
+        ```python
+        class MyPage(Page):
+            def forward(self):
+                # your implementation here
+        ```
     """
 
     def __init__(self, bot: Bot, page_name: str = 'page_name'):
         """
-        Initialize the page class.
+        Initializes the Page class.
 
-        In the locators file must be declared the page in the below format:
-
+        In the locators file, the page must be declared in the format:
         [pages_url]
         page_name=https://example.com/page
+
+        Args:
+            bot (Bot): The bot instance associated with the page.
+            page_name (str): The name of the page.
+
+        Raises:
+            ValueError: If the locator is not enclosed in round brackets.
         """
         super().__init__()
 
@@ -42,29 +63,42 @@ class Page(ABC):
 
     @property
     def bot(self):
+        """
+        Gets the associated bot instance.
+
+        Returns:
+            Bot: The bot instance.
+        """
         return self._bot
 
     def __locator__(self, locator_name: str) -> tuple:
         """
-        Locator
+        Utility method to load a locator.
 
-        Utility used to load the locator.
-
-        The locators in the file must be in the below format:
-
+        The locators in the file must be in the format:
         [page_name]
         locator_name=(By.XPATH, "//html//input")
 
+        Args:
+            locator_name (str): The name of the locator.
+
+        Returns:
+            tuple: A tuple representing the loaded locator.
+
+        Raises:
+            ValueError: If the locator is not enclosed in round brackets or is of an unknown or incorrect format.
         """
-        # load the locators from file and interprete that as code
+        # load the locators from file and interpret that as code
         full_locator: str = self._bot.locator(self._page_name, locator_name)
 
         if not full_locator.startswith('(') or not full_locator.endswith(')'):
             raise ValueError('The locator must be enclosed in round brackets.')
 
         # declared locators
-        locator_list: List[str] = ['By.ID', 'By.XPATH', 'By.NAME', 'By.CLASS_NAME', 'By.CSS_SELECTOR', 
-                                       'By.LINK_TEXT', 'By.PARTIAL_LINK_TEXT', 'By.TAG_NAME']
+        locator_list: List[str] = [
+            'By.ID', 'By.XPATH', 'By.NAME', 'By.CLASS_NAME', 'By.CSS_SELECTOR', 
+            'By.LINK_TEXT', 'By.PARTIAL_LINK_TEXT', 'By.TAG_NAME'
+        ]
 
         # check the used locator
         parsed_locator: tuple = None
@@ -72,22 +106,27 @@ class Page(ABC):
             # check that the first characters are them of the locators and the next one of the comma 
             if full_locator[1:-1].strip().startswith(locator) and full_locator[1:-1].strip()[len(locator):].strip().startswith(','):
                 # extract the tuple required as locator
-                parsed_locator = (eval(locator), full_locator[1:-1].strip()[len(locator):].strip()[1:].strip()[1:-1])
+                parsed_locator = (
+                    eval(locator), 
+                    full_locator[1:-1].strip()[len(locator):].strip()[1:].strip()[1:-1]
+                )
 
                 logging.info(f'{parsed_locator}')
 
                 return parsed_locator
             
         else:
-            raise ValueError('The specified locator is unknown or worng, check by, brackets and commas.')
+            raise ValueError('The specified locator is unknown or wrong; check by, brackets, and commas.')
 
     @abstractmethod
     def forward(self) -> Union[Type['Page'], None]:
         """
-        Forward
+        Represents a series of actions on the page.
 
-        This method represents a series of action in one page,
-        in order to pass on another page or when the task it's done
-        it will return None, in order to finish.
+        Returns:
+            Union[Type['Page'], None]: Either the next page instance or None to finish.
+
+        Raises:
+            NotImplementedError: Subclasses must define this method.
         """
         raise NotImplementedError('Tasks must define this method.')
