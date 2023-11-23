1f45511afe171d21df2f7716478598605dea2c1d
Davide Uberti <24529587+ubertidavide@users.noreply.github.com>
2023-11-23 20:16:11 +0100

    Update task.py

--- fastbots/task.py
+++ fastbots/task.py
@@ -8,66 +8,93 @@ from fastbots.chrome_bot import ChromeBot
 from fastbots.firefox_bot import FirefoxBot
 from fastbots import config
 
-
 logger = logging.getLogger(__name__)
-
 
 class Task(ABC):
     """
     Task
 
-    A Task blueprint that need to implement the three below specified methods.
-    It is a class needed to rapresent a full interaction over more pages.
+    A blueprint for tasks representing a series of interactions across multiple pages.
+
+    Methods:
+        run(bot: Bot) -> bool: Executes the series of interactions. Must be implemented by subclasses.
+        on_success(payload): Actions to be taken on successful completion of the run method.
+        on_failure(payload): Actions to be taken if the run method fails after a specified number of retries.
     """
 
     @abstractmethod
     def run(self, bot: Bot) -> bool:
         """
-        Run
+        Executes the series of interactions.
 
-        This method it's called when the class is istantiated, don't call
-        them directly, used for blueprint of a series of interaction with
-        different pages in order to make a complete navigation.
+        Args:
+            bot (Bot): The bot instance to perform interactions.
+
+        Returns:
+            bool: True if interactions are successful, False otherwise.
+
+        Raises:
+            NotImplementedError: Subclasses must define this method.
         """
         raise NotImplementedError('Tasks must define this method.')
     
     @abstractmethod
     def on_success(self, payload):
         """
-        On Success
+        Actions to be taken on successful completion of the run method.
 
-        This method is executed automatically only if the run
-        method is runned whitout problems.
+        Args:
+            payload: Data collected during the run method.
+
+        Raises:
+            NotImplementedError: Subclasses must define this method.
         """
         raise NotImplementedError('Tasks must define this method.')
 
     @abstractmethod
     def on_failure(self, payload):
         """
-        On Failure
+        Actions to be taken if the run method fails after a specified number of retries.
 
-        This method is executed automatically only if the run
-        method is runned with problems for a specified series of times.
+        Args:
+            payload: Data collected during the run method.
+
+        Raises:
+            NotImplementedError: Subclasses must define this method.
         """
         raise NotImplementedError('Tasks must define this method.')
     
     def __is_false__(self, value):
-        """Return True if value is False"""
+        """
+        Returns True if the value is False.
+
+        Args:
+            value: The value to check.
+
+        Returns:
+            bool: True if the value is False, False otherwise.
+        """
         return value is False
     
     def __call__(self):
         """
-        This method is runned automatically when this class it's istantiated
-        and called, it will execute the run method with all it' correct logic.
+        Automatically executed when the class is instantiated and called.
+        
+        It executes the run method with appropriate logic and handles retries.
+
+        Returns:
+            bool: True on success, False on failure after retries.
         """
         result: bool = False
         payload: dict = {}
 
         try:
-            for attempt in Retrying(wait=wait_fixed(config.BOT_RETRY_DELAY),
-                                    stop=stop_after_attempt(config.BOT_MAX_RETRIES),
-                                    retry=retry_if_result(self.__is_false__),
-                                    after=after_log(logger, logging.DEBUG)):
+            for attempt in Retrying(
+                wait=wait_fixed(config.BOT_RETRY_DELAY),
+                stop=stop_after_attempt(config.BOT_MAX_RETRIES),
+                retry=retry_if_result(self.__is_false__),
+                after=after_log(logger, logging.DEBUG)
+            ):
                 with attempt:
                     bot: Bot = None
 
