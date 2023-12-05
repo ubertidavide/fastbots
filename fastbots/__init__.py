b2ed3ffbaea60cda5020c543404a8fe565a28681
Davide Uberti <24529587+ubertidavide@users.noreply.github.com>
2023-12-05 21:18:24 +0100

    Update __init__.py

--- fastbots/__init__.py
+++ fastbots/__init__.py
@@ -3,7 +3,9 @@ from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains 
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import TimeoutException, NoSuchElementException, ElementNotInteractableException, StaleElementReferenceException, ElementClickInterceptedException
 
 from fastbots.bot import Bot
 from fastbots.page import Page
